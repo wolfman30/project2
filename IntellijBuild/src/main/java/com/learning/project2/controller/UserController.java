15dc926191ab568c7ee9306f9ef5f9283740d4a4
ARosenbaum98 <90282324+ARosenbaum98@users.noreply.github.com>
2021-10-14 18:37:26 -0500

    Backend Updates

---
+++ IntellijBuild/src/main/java/com/learning/project2/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.learning.project2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "user")
+public class UserController {
+}
