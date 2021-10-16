764108de027edec19e35ea182c968ba41c20b36e
Andrew John Wolf <wolf47@email.franklin.edu>
2021-10-16 14:07:58 -0400

    created Quiz class

---
+++ AngularBuild/src/app/models/quiz.ts
@@ -0,0 +1,27 @@
+import { QuizConfiguration } from './quiz-configuration'; 
+import { Question } from './questionClass'; 
+
+export class Quiz 
+{
+    id: number = 0; 
+    subject: string = ''; 
+    test_name: string = ''; 
+    config: QuizConfiguration = new QuizConfiguration(0); 
+    questions: Question[] = []; 
+
+    constructor(data: any)
+    {
+        if (data)
+        {
+            this.id = data.id; 
+            this.subject = data.subject; 
+            this.test_name = data.test_name; 
+            this.config = new QuizConfiguration(data.config); 
+            this.questions = []; 
+            data.questions.forEach( (query:any) =>
+            {
+                this.questions.push(new Question(query)); 
+            }); 
+        }
+    }
+}
