6081301814c91a22c42722ae79042a1a4386659d
ARosenbaum98 <90282324+ARosenbaum98@users.noreply.github.com>
2021-10-25 11:18:13 -0500

    Simplified bot/converse API

---
+++ IntellijBuild/src/main/java/com/learning/project2/web/lex/models/Response.java
@@ -0,0 +1,15 @@
+package com.learning.project2.web.lex.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+
+    private String sessionId;
+    private String message;
+
+}
