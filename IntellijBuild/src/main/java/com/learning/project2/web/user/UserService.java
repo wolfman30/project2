beeb9b2e1a258f34e8a0760307a98bbc4a1aedd6
ARosenbaum98 <90282324+ARosenbaum98@users.noreply.github.com>
2021-10-25 10:23:06 -0500

    Decoupled User service/controller

---
+++ IntellijBuild/src/main/java/com/learning/project2/web/user/UserService.java
@@ -0,0 +1,56 @@
+package com.learning.project2.web.user;
+
+import org.hibernate.JDBCException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    private void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+    public ResponseEntity<User> createOrUpdate(User user) {
+        try{
+            System.out.println(user.toString());
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }catch(JDBCException | DataIntegrityViolationException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<User> loginAttempt(User user) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            System.out.println(user.getUsername());
+            String username = user.getUsername();
+            String password = user.getPassword();
+
+            User login = userRepository.findByUsernameIgnoreCaseAndPassword(username, password);
+
+            if (login != null) {
+                return new ResponseEntity<>(login, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
